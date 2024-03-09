da9cef7b386a235830c461363608c924fc69fc1a
JFabianGH <fabian.godines.h@gmail.com>
2024-03-09 16:08:21 -0600

    Cambios

--- src/main/java/com/ebc/test/Controller/CollatzController.java
+++ src/main/java/com/ebc/test/Controller/CollatzController.java
@@ -1,13 +1,13 @@
 package com.ebc.test.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CollatzController {
     @GetMapping("/collatz")
-    public String getCollatzSequence(@RequestParam("number") int number) {
+    public String getCollatzSequence(@RequestParam int number) {
         StringBuilder sequence = new StringBuilder();
         int n = number;
         while (n != 1) {
@@ -20,5 +20,22 @@ public class CollatzController {
         }
         sequence.append(1); // Add the final value (1)
         return sequence.toString();
+    }
+
+    @PutMapping("/api/sequence/collatz/{input}")
+    public ResponseEntity<String> putCollatzSequence(@PathVariable("input") int input){
+        StringBuilder sequence = new StringBuilder();
+        int n = input;
+        while (n != 1) {
+            sequence.append(n).append(" -> ");
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n = 3 * n + 1;
+            }
+        }
+        sequence.append(1);
+        String jsonResult = "{\"sequence\": \"" + sequence.toString() + "\"}";
+        return new ResponseEntity<>(jsonResult, HttpStatus.OK);
     }
 }
--- src/main/java/com/ebc/test/Controller/FizzBuzzController.java
+++ src/main/java/com/ebc/test/Controller/FizzBuzzController.java
@@ -1,7 +1,7 @@
 package com.ebc.test.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +19,7 @@ public class FizzBuzzController {
             return String.valueOf(number);
         }
     }
+
+
 
 }
